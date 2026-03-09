b8230527b8197348d0d95022be019bba678fd298
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-09 12:17:12 +0000

    e

--- session1/task3/task3.c
+++ session1/task3/task3.c
@@ -5,44 +5,8 @@
 #include <stdlib.h>
 #include "logging.h"
 
-
-int* get_values(int argc, char* argv[])
-{
-    LOG_FMT("get_values(): argc=%d\n", argc);
-
-    int value;
-    int* values = NULL;
-
-    if (argc > 1) {
-        values = calloc(argc - 1, sizeof(int));
-        for (int i = 0; i < argc - 1; ++i) {
-            value = atoi(argv[i + 1]);
-            LOG_FMT("get_values(): arg \"%s\" -> %d\n", argv[i + 1], value);
-            values[i] = value;
-        }    
-    }
-
-    LOG_FMT("get_values(): returning %p\n", values);
-
-    return values;
-}
-
-
-long sum_values(int data[], int n)
-{
-    assert(data != NULL);
-    assert(n > 0);
-
-    long result = 0L;
-
-    for (int i = 0; i < n; ++i) {
-        result += data[i];
-        LOG_FMT("sum_values(): i=%d, sum=%ld\n", i, result);
-    }
-
-    return result;
-}
-
+int* get_values(int argc, char* argv[]);
+long sum_values(int data[], int n);
 
 int main(int argc, char* argv[])
 {
@@ -66,4 +30,40 @@ int main(int argc, char* argv[])
     printf("Sum of values = %ld\n", sum);
 
     return 0;
+}
+
+int* get_values(int argc, char* argv[])
+{
+    LOG_FMT("get_values(): argc=%d\n", argc);
+
+    int value;
+    int* values = NULL;
+
+    if (argc > 1) {
+        values = calloc(argc - 1, sizeof(int));
+        for (int i = 0; i < argc - 1; ++i) {
+            value = atoi(argv[i + 1]);
+            LOG_FMT("get_values(): arg \"%s\" -> %d\n", argv[i + 1], value);
+            values[i] = value;
+        }    
+    }
+
+    LOG_FMT("get_values(): returning %p\n", values);
+
+    return values;
+}
+
+long sum_values(int data[], int n)
+{
+    assert(data != NULL);
+    assert(n > 0);
+
+    long result = 0L;
+
+    for (int i = 0; i < n; ++i) {
+        result += data[i];
+        LOG_FMT("sum_values(): i=%d, sum=%ld\n", i, result);
+    }
+
+    return result;
 }
